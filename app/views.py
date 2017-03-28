a8b4f54bfe2d228895f39896c5657be3a94f5bdd
Echo Wu <echo.g.wu@gmail.com>
2017-03-28 12:04:44 -0700

    complete part 2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Echo'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'Ryan'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", user = user, posts = posts)
